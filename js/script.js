2b528061659f4e5b2c8b8c188432de28102fc13d
yuki-fujisaki <yuki.f.bizz@gmail.com>
2022-08-24 15:50:05 +0900

    [finish] デバッグ完了

--- js/script.js
+++ js/script.js
@@ -11,14 +11,14 @@ $(function () {
 
     // 試行回数
     trials++;
-    $('.trials').html(trial);
+    $('.trials').html(trials);
 
     const VAL_NUMBER = $(this).val();
     let val = Number(VAL_NUMBER) || 0; // NaNのとき0にする
-    const RESULT = Math.abs(val - run); //数字がどのぐらい離れているか
+    const RESULT = Math.abs(val - ran); //数字がどのぐらい離れているか
 
     //判定処理
-    if (val > 0 || val < 9) {
+    if (val < 0 || val > 9) {
       $('.result-text').html("範囲内で入力してね！");
     } else if (RESULT == 0) {
       $('.result-text').html("正解!");
